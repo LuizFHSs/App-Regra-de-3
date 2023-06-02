b89856881019935c116bdd50b73814640fea3e0c
LuizFHSs <devlfhss@gmail.com>
2023-06-02 17:29:16 -0300

    update-algoritmo.cs

--- App_R3/Algoritmo.cs
+++ App_R3/Algoritmo.cs
@@ -26,7 +26,7 @@ namespace App_R3
             float result = 0;
 
             // regra de tres simples direta
-            if (v4 == "x" && regra[0]) 
+            if (v4.ToLower() == "x" && regra[0]) 
             {
                 mult1 = float.Parse(v1) * value4;
                 mult2 = float.Parse(v2) * float.Parse(v3);
@@ -35,7 +35,7 @@ namespace App_R3
             }
             else
             {
-                if(v3 == "x" && regra[0])
+                if(v3.ToLower() == "x" && regra[0])
                 {
                     mult1 = float.Parse(v2) * value3;
                     mult2 = float.Parse(v1) * float.Parse(v4);
@@ -45,7 +45,7 @@ namespace App_R3
             }
 
             // regra de tres simples inversa
-            if (v4 == "x" && regra[1]) 
+            if (v4.ToLower() == "x" && regra[1]) 
             {
                 mult1 = float.Parse(v3) * value4;
                 mult2 = float.Parse(v1) * float.Parse(v2);
@@ -54,7 +54,7 @@ namespace App_R3
             }
             else
             {
-                if(v3 == "x" && regra[1]) 
+                if(v3.ToLower() == "x" && regra[1]) 
                 {
                     mult1 = float.Parse(v4) * value3;
                     mult2 = float.Parse(v1) * float.Parse(v2);
@@ -65,20 +65,192 @@ namespace App_R3
             return result;
         }
 
-        public float Composta(bool regra, string v1, string v2, string v3, string v4, string v5, string v6)
+        public float Composta(bool[] regra, string v1, string v2, string v3, string v4, string v5, string v6)
         {
-            float value4 = 0;
+            float value4 = 1;
+            float value5 = 1;
+            float value6 = 1;
             float mult1;
             float mult2;
+            float mult3;
+            float mult4;
             float div;
             float result = 0;
 
             if(v4.ToLower() == "x")
             {
+                if(regra[0] && regra[1])
+                {
+                    mult1 = float.Parse(v2) * float.Parse(v3);
+                    mult2 = float.Parse(v5) * float.Parse(v6);
 
+                    mult3 = float.Parse(v1) * mult2;
+                    mult4 = mult1 * value4;
+
+                    div = mult3 / mult4;
+                    result = div;
+                }
+                else
+                {
+                    if (!regra[0] && regra[1])
+                    {
+                        mult1 = float.Parse(v5) * float.Parse(v3);
+                        mult2 = float.Parse(v2) * float.Parse(v6);
+
+                        mult3 = float.Parse(v1) * mult2;
+                        mult4 = mult1 * value4;
+
+                        div = mult3 / mult4;
+                        result = div;
+                    }
+                    else
+                    {
+                        if(regra[0] && !regra[1])
+                        {
+                            mult1 = float.Parse(v2) * float.Parse(v6);
+                            mult2 = float.Parse(v5) * float.Parse(v3);
+
+                            mult3 = float.Parse(v1) * mult2;
+                            mult4 = mult1 * value4;
+
+                            div = mult3 / mult4;
+                            result = div;
+                        }
+                        else
+                        {
+                            if(!regra[0] && !regra[1])
+                            {
+                                mult1 = float.Parse(v5) * float.Parse(v6);
+                                mult2 = float.Parse(v2) * float.Parse(v3);
+
+                                mult3 = float.Parse(v1) * mult2;
+                                mult4 = mult1 * value4;
+
+                                div = mult3 / mult4;
+                                result = div;
+                            }
+                        }
+                    }
+                }
             }
+            else
+            {
+                if (v5.ToLower() == "x")
+                {
+                    if (regra[0] && regra[1])
+                    {
+                        mult1 = float.Parse(v1) * float.Parse(v3);
+                        mult2 = float.Parse(v4) * float.Parse(v6);
 
+                        mult3 = float.Parse(v2) * mult2;
+                        mult4 = mult1 * value5;
 
+                        div = mult3 / mult4;
+                        result = div;
+                    }
+                    else
+                    {
+                        if (!regra[0] && regra[1])
+                        {
+                            mult1 = float.Parse(v4) * float.Parse(v3);
+                            mult2 = float.Parse(v1) * float.Parse(v6);
+
+                            mult3 = float.Parse(v2) * mult2;
+                            mult4 = mult1 * value5;
+
+                            div = mult3 / mult4;
+                            result = div;
+                        }
+                        else
+                        {
+                            if (regra[0] && !regra[1])
+                            {
+                                mult1 = float.Parse(v1) * float.Parse(v6);
+                                mult2 = float.Parse(v4) * float.Parse(v3);
+
+                                mult3 = float.Parse(v2) * mult2;
+                                mult4 = mult1 * value5;
+
+                                div = mult3 / mult4;
+                                result = div;
+                            }
+                            else
+                            {
+                                if (!regra[0] && !regra[1])
+                                {
+                                    mult1 = float.Parse(v4) * float.Parse(v6);
+                                    mult2 = float.Parse(v1) * float.Parse(v3);
+
+                                    mult3 = float.Parse(v2) * mult2;
+                                    mult4 = mult1 * value5;
+
+                                    div = mult3 / mult4;
+                                    result = div;
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (v6.ToLower() == "x")
+                    {
+                        if (regra[0] && regra[1])
+                        {
+                            mult1 = float.Parse(v1) * float.Parse(v2);
+                            mult2 = float.Parse(v4) * float.Parse(v5);
+
+                            mult3 = float.Parse(v3) * mult2;
+                            mult4 = mult1 * value6;
+
+                            div = mult3 / mult4;
+                            result = div;
+                        }
+                        else
+                        {
+                            if (!regra[0] && regra[1])
+                            {
+                                mult1 = float.Parse(v4) * float.Parse(v2);
+                                mult2 = float.Parse(v1) * float.Parse(v5);
+
+                                mult3 = float.Parse(v3) * mult2;
+                                mult4 = mult1 * value6;
+
+                                div = mult3 / mult4;
+                                result = div;
+                            }
+                            else
+                            {
+                                if (regra[0] && !regra[1])
+                                {
+                                    mult1 = float.Parse(v1) * float.Parse(v5);
+                                    mult2 = float.Parse(v4) * float.Parse(v2);
+
+                                    mult3 = float.Parse(v3) * mult2;
+                                    mult4 = mult1 * value6;
+
+                                    div = mult3 / mult4;
+                                    result = div;
+                                }
+                                else
+                                {
+                                    if (!regra[0] && !regra[1])
+                                    {
+                                        mult1 = float.Parse(v4) * float.Parse(v5);
+                                        mult2 = float.Parse(v1) * float.Parse(v2);
+
+                                        mult3 = float.Parse(v3) * mult2;
+                                        mult4 = mult1 * value6;
+
+                                        div = mult3 / mult4;
+                                        result = div;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             return result;
         }
     }
