6059adf8029a07b3510df538237668ec426aee0c
LuizFHSs <devlfhss@gmail.com>
2023-06-02 12:42:47 -0300

    update-files

--- App_R3/Form1.Designer.cs
+++ App_R3/Form1.Designer.cs
@@ -270,7 +270,7 @@
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(198, 87);
+            this.tabControl1.Size = new System.Drawing.Size(221, 87);
             this.tabControl1.TabIndex = 24;
             this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
             // 
@@ -281,7 +281,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 24);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(190, 59);
+            this.tabPage1.Size = new System.Drawing.Size(213, 59);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Simples";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -313,7 +313,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 24);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(190, 59);
+            this.tabPage2.Size = new System.Drawing.Size(213, 59);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Composta";
             this.tabPage2.UseVisualStyleBackColor = true;
